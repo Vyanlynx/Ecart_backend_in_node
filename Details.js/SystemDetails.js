5b8ad0a6112d51b4349df5d10ef968ad2e07df0a
RAGHUL-R01 <raghulnishanth195@gmail.com>
2022-08-24 12:34:58 +0530

    json

--- Details.js/SystemDetails.js
+++ Details.js/SystemDetails.js
@@ -1,29 +1,38 @@
+const { json } = require('body-parser');
 const express = require('express');
 const route = express();
-// const Post_details = require('./DetailsDB');
+const Post_details_json = require('../datas.json');
+const fs = require('fs');
 const Saved_data = []
+var read_file = fs.readFileSync("datas.json");
+var data_details = JSON.parse(read_file);
+route.get('/store', async (request, response) => {
 
-route.get('/serverdetails', async (request, response) => {
-   var ip = request.headers['x-forwarded-for'] ||
-     request.socket.remoteAddress ||
-     null;
-     console.log("first")
-   // var Post_details_DB = new Post_details({
-   //  latitude:request.params.latitude,
-   //  longitude:request.params.longitude,
-   //  IP:request.params.IP,
-   //  postal:request.params.postal
-   // });
-   // try{
-   // const saved = await Post_details_DB.save();
-   // response.status(200).send();
-   //  }
-   // catch(err){
-   //  response.send("Error");
-   // }
-   Saved_data.push( ip );
-   // console.log(request.ip);
-   response.json(Saved_data);
-   // Take server IP details and username password details to front end admin id.
+  // var Post_details_DB = new Post_details({
+  //  latitude:request.params.latitude,
+  //  longitude:request.params.longitude,
+  //  IP:request.params.IP,
+  //  postal:request.params.postal
+  // });
+  // try{
+  // const saved = await Post_details_DB.save();
+  // response.status(200).send();
+  //  }
+  // catch(err){
+  //  response.send("Error");
+  // }
+  //  Saved_data.push( ip );
+  var ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress || null;
+
+  data_details[Date()] = ip;
+  fs.writeFile('datas.json', JSON.stringify(data_details), (err) => {
+    if (err) response.json(err);
+  })
+  // console.log(request.ip);
+  response.json("Welcome");
+  // Take server IP details and username password details to front end admin id.
+})
+route.get("/show", (req, res) => {
+  res.json(JSON.parse(fs.readFileSync("datas.json")));
 })
 module.exports = route;
