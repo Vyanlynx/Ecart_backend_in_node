87afa1761f54802c6cc83f6c149e242f95bb963f
RAGHUL-R01 <raghulnishanth195@gmail.com>
2022-08-25 18:14:42 +0530

    SOme changes in route file

--- Routes/DBmodel.js/DBschema.js
+++ Routes/DBmodel.js/DBschema.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 
 const PostSchema = mongoose.Schema({
-    UserName:{
-        type:String
+    UserName: {
+        type: String
     },
-    Password:{
-        type:String
+    Password: {
+        type: String
+    },
+    Contact: { 
+        type: String 
+    },
+    Address: { 
+        type: String 
+    },
+    Password_without_hash: { 
+        type: String 
     }
 });
 
-module.exports = mongoose.model('PostData',PostSchema);
+module.exports = mongoose.model('PostData', PostSchema);
--- Routes/Route.js
+++ Routes/Route.js
@@ -1,56 +1,47 @@
 const Express = require('express');
 const Route = Express.Router(); //Router instance is needed to run the routes in EXpress
 const PostData = require('./DBmodel.js/DBschema'); // Importing Database model and schema
-const DB_ref = require('../Routes/DBmodel.js/DBschema_for_ref');
 const bcrypt = require('bcryptjs'); // Hashing the password before sending to DB and After receiving from DB. 
 const Jwt = require('jsonwebtoken'); // JWT Autherization 
 // const { json } = require('body-parser');
 
 require("dotenv").config();
 
-// Sending User name and password to client for testing purpose:
-Route.get("/ADMIN", async (request, response) => {
-    const Posting_data = new DB_ref({  // Create Object for MongoDB model.
-        User__Name: request.body.UserName,
-        Password__: hashed_Password
-    });
-    try {
-        const saved = await Posting_data.save(); //Method in mongoose to send the data to MongoDB
-        response.json("Good");
-    }
-    catch (err) {
-        response.json("Some error");
-    }
-})
-
 //check if the user name is valid;
 Route.get('/:name/:Password', async (request, response) => {
     try {
         const Check = await PostData.findOne({ UserName: request.params.name }); // check if user is already available findOne is a moongoose method to find that query in databage in all collection.
-        if (Check == "") {
+        if (Check === null || Check === "") {
             return response.status(400).json("User is not found");
         }
         else {
             try {
-                const Verify_Password = await PostData.findOne({ UserName: request.params.name });
+                // const Verify_Password = await PostData.findOne({ UserName: request.params.name });
+                console.log("first")
                 /*In bcrypt compare 1st parameter is without hash, second is hashed with salt */
-                if (await bcrypt.compare(request.params.Password, Verify_Password.Password)) //comparing Password from Database
+                if (await bcrypt.compare(request.params.Password, Check.Password)) //comparing Password from Database
                 {
-                    const user_token = Jwt.sign(request.params.name, process.env.SECRET_KEY); //sign to used to create token
-                    return response.cookie("accesstoken",JSON.stringify(user_token)).send("Logged in Successfully");
+                    console.log("inside bcypt")
+                    const user_token = Jwt.sign({Name:request.params.name}, process.env.SECRET_KEY,{ expiresIn: 8000}); //sign to used to create token
+                    console.log(" bcypt")
+                    return response.cookie("accesstoken",JSON.stringify(user_token)).json("Logged in Successfully");
+
+                    // return response.setHeader('Authorization',JSON.stringify(user_token));
                     // return response.json(user_token);
                     // return response.status(200).json("Logged in Successfully");
                 }
                 else {
-                    return response.status(401).json("please check your password");
+                    console.log("inslde else")
+                    return response.status(400).json("please check your password");
                 }
+                console.log("end")
             } catch (error) {
-                return response.status(100).json("Server is busy");
+                return response.status(400).json("Server busy!");
             }
         }
     }
     catch (err) {
-        return response.json(err);
+        return response.status(400).json(err);
     }
     // *******************************************************************************************************************
 
@@ -84,10 +75,15 @@ Route.post('/', async (request, response) => {
     // --------------------------------------------------------------------------------------------------------------------------
 
     const User_data = await PostData.findOne({ UserName: request.body.UserName });
+    const User_mail = await PostData.findOne({ Contact: request.body.Contact });
     // console.log(User_data);
     // if (User_data.some((User) => { return User.UserName === request.body.UserName })) {
     if (User_data !== null) {
         response.json("User name has already taken");
+    }
+    else if(User_mail !== null){
+        response.json("User Mail Id is already Existing");
+
     }
     else {
         // (User_data.some((User)=> {console.log((User.UserName == request.body.UserName))}));
@@ -96,22 +92,17 @@ Route.post('/', async (request, response) => {
         const Posting_data = new PostData({  // Create Object for MongoDB model.
             UserName: request.body.UserName,
             Password: hashed_Password,
-            mail: request.body.email,
+            Password_without_hash: request.body.Password,
+            Contact: request.body.Contact,
             Address: request.body.address
         });
-        
-    const Posting_data_ref = new DB_ref({  // Create Object for MongoDB model.
-        UserName: request.body.UserName,
-        Password: request.body.Password
-    });
 
         try {
             const saved = await Posting_data.save(); //Method in mongoose to send the data to MongoDB
-            const saved1 = await Posting_data_ref.save(); //Method in mongoose to send the data to MongoDB
             response.status(202).json("Account Created Successfully");
         }
         catch (error) {
-            response.status(404).json("Server busy!");
+            response.json("Server busy!");
         }
 
         // MongoDB is working slow, So storing values in server side, it's not safe but we don't have any other choices.
